d97b23062fb8245bdb54349b034a71bbf9713155
dennisvg111 <dennisvg111@gmail.com>
2017-03-21 14:02:48 +0100

    $fix(browser) fixed a minor issue with fn.set_limits - preview displacement was caused by wrongfully resized widgets, - * this.options.distance wasn't actually needed

--- src/jquery.draggable.js
+++ src/jquery.draggable.js
@@ -212,7 +212,7 @@
     fn.set_limits = function(container_width) {
         container_width || (container_width = this.$container.width());
         this.player_max_left = (container_width - this.player_width +
-            -this.options.offset_left * this.options.distance);
+            -this.options.offset_left);
         this.player_max_top = (this.options.container_height - this.player_height +
             -this.options.offset_top);
 
